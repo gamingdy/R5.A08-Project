7c0a6e4236e57569a82345504c514357f3f7327d
username-Everam <simsimi.armand@gmail.com>
2024-12-09 15:56:21 +0100

    #6 Update main with all TVA

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 
 package org.example.montaine.guillaume
-import org.example.montaine.guillaume.megamangashop.MangaLot
+import org.example.montaine.guillaume.megamangashop.Boutique
 import org.example.montaine.guillaume.megamangashop.Pays
 import org.example.montaine.guillaume.megamangashop.TicketDeCaisse
 
 fun main() {
 
-    val Commande = TicketDeCaisse(pays= Pays.France)
+    val Commande1 = TicketDeCaisse(pays= Pays.France)
     println("\u001B[1mBienvenue dans le calcul de votre commande\u001B[0m\n")
     println("Veuillez choisir le pays de livraison :")
     println("1 - France")
@@ -15,17 +15,17 @@ fun main() {
     println("4 - Royaume-Uni")
     println("5 - Belgique")
     val choix = readln().toInt()
-    var paysBoutique = Pays.France
+    var paysBoutique = Pays.France //Pays par défaut pour éviter les erreurs
     if (choix == 1) {
         paysBoutique = Pays.France
-    //} else if (choix == 2) {
-    //    val pays = Pays.Espagne
-    //} else if (choix == 3) {
-    //    val pays = Pays.Allemagne
-    //} else if (choix == 4) {
-    //    val pays = Pays.RoyaumeUni
-    //} else if (choix == 5) {
-    //    val pays = Pays.Belgique
+    } else if (choix == 2) {
+        val pays = Pays.Espagne
+    } else if (choix == 3) {
+        val pays = Pays.Allemagne
+    } else if (choix == 4) {
+        val pays = Pays.RoyaumeUni
+    } else if (choix == 5) {
+        val pays = Pays.Belgique
     }
     val Boutique = Boutique(pays = paysBoutique)
 
@@ -41,6 +41,7 @@ fun main() {
         val total = lot.calculatePrixLot()
         println("Le sous-total du lot $i est de \u001B[1m$total\u001B[0m\n")
     }
-    val totalHT = Commande.calculateTotalHT()
-    println("Le total de votre commande est de \u001B[1m$totalHT\u001B[0m")
+    val totalTTC = Commande.calculerTotalTTC()
+    println("Le total de votre commande est de \u001B[1m$totalTTC\u001B[0m")
+    println(Commande.afficherTicket())
 }
