a3e75abc0f569bb1703e93e91aca78c61f0c77a8
Sofia Gribanova <grisoffa@gmail.com>
2024-12-09 15:12:23 +0100

    Update forgotten tests

--- src/test/kotlin/org/mangashop/tests/TestPrixBasique.kt
+++ src/test/kotlin/org/mangashop/tests/TestPrixBasique.kt
@@ -25,6 +25,51 @@ class TestPrixBasique : BehaviorSpec({
                 }
             }
         }
+        given("Une quantité negative et un prix")
+        {
+            val lot = MangaLot()
+            val quantite = -3
+            val prix = 10.0
+
+            When("je calcule le prix total") {
+                try{
+                    lot.calculateLot(quantite, prix)
+                } catch (e: IllegalArgumentException) {
+                    Then("une exception est levée") {
+                        e.message shouldNotBe "La quantité ne doit pas être negative"
+                    }
+                }
+            }
+        }
+        given("Je donne un prix negatif"){
+            val lot = MangaLot()
+            val quantite = 3
+            val prix = -10.0
+
+            When("je calcule le prix total") {
+                try{
+                    lot.calculateLot(quantite, prix)
+                } catch (e: IllegalArgumentException) {
+                    Then("une exception est levée") {
+                        e.message shouldNotBe "Le prix ne doit pas être negatif"
+                    }
+                }
+            }
+        }
+        given ("Je donne une quantité a 0")
+        {
+            val lot = MangaLot()
+            val quantite = 0
+            val prix = 10.0
+
+            When("je calcule le prix total") {
+                val total = lot.calculateLot(quantite, prix)
+
+                Then("le prix total est 0") {
+                    total shouldBe 0.0
+                }
+            }
+        }
     }
 
 }
