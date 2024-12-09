475073b55bf1b981658babfd468a333a6c04110b
username-Everam <simsimi.armand@gmail.com>
2024-12-09 15:10:15 +0100

    Update Main.kt
    Ajout des calculs de prix pour plusieurs lots différents

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,20 @@
 
 package org.example.montaine.guillaume
 
+import org.example.montaine.guillaume.megamangashop.MangaLot
+
 fun main() {
-    println("Hello World!")
+
+    println("\u001B[1mBienvenue dans le calcul de votre commande\u001B[0m\n")
+    println("Veuillez rentrer le nombre de lots différents que vous avez dans votre commande :")
+    val nbLot = readln().toInt()
+    for (i in 1..nbLot) {
+        println("Entrez la quantité de manga du lot $i")
+        val quantite = readln().toInt()
+        println("Entrez le prix unitaire du manga")
+        val prix = readln().toDouble()
+        val lot = MangaLot()
+        val total = lot.calculateLot(quantite, prix)
+        println("Le total du lot $i est de $total")
+    }
 }
