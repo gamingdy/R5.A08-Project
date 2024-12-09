08563ca34cbc7466b205138bdab3f1ea10f685b6
Gamingdy <christgik12@gmail.com>
2024-12-09 15:30:35 +0100

    #2 ajout du calcul ttc

--- src/main/kotlin/megamangashop/TicketDeCaisse.kt
+++ src/main/kotlin/megamangashop/TicketDeCaisse.kt
@@ -4,11 +4,11 @@ package org.example.montaine.guillaume.megamangashop
 import de.huxhorn.sulky.ulid.ULID;
 
 
-data class TicketDeCaisse(val Id: String = ULID().nextULID() ) {
+data class TicketDeCaisse(val Id: String = ULID().nextULID(), val pays:Pays ) {
 
     private var totalTtc: Double = 0.0
     private var totalHT: Double = 0.0
-    private val taxe: Double = 0.0
+    private val taxe: Double = pays.tva
     private val remise: Double = 0.0
     private var lots : MutableList<MangaLot> = mutableListOf()
 
@@ -24,6 +24,11 @@ data class TicketDeCaisse(val Id: String = ULID().nextULID() ) {
         val lot = MangaLot(quantite, prix)
         lots.add(lot)
         return lot
+    }
+
+    fun calculerTotalTTC(): Double {
+        totalTtc = totalHT * (1 + taxe)
+        return totalTtc
     }
 
     fun afficherTicket(): String {
