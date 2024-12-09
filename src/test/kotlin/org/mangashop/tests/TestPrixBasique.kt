e6d932ad8d147dd1c4e52a147cbc234a645af6ea
Sofia Gribanova <grisoffa@gmail.com>
2024-12-09 14:13:52 +0100

    Premier test

--- src/test/kotlin/org/mangashop/tests/TestPrixBasique.kt
+++ src/test/kotlin/org/mangashop/tests/TestPrixBasique.kt
@@ -2,28 +2,26 @@
 package org.mangashop.tests
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import org.example.montaine.guillaume.megamangashop.TicketDeCaisse
 
 class TestPrixBasique : BehaviorSpec({
 
-    context("je veux vérifier que chaque ticket a son identifiant unique") {
+    context("A partir d'une quantité de manga je veux un prix total") {
 
-        given("un ticket") {
-            val ticket = TicketDeCaisse()
+        given("Une quantité et un prix") {
+            val quantite = 3
+            val prix = 10.0
 
-            And("un 2e ticket") {
-                val ticket2 = TicketDeCaisse()
+            When("je calcule le prix total") {
+                val total = calculateLot(quantite, prix)
 
-                When("je lis les 2 identifiants") {
-                    val id1 = ticket.Id
-                    val id2 = ticket2.Id
-
-                    Then("ce n'est pas le même") {
-                        id1 shouldNotBe id2
-                    }
-
+                Then("le prix total est le bon") {
+                    total shouldBe 30.0
                 }
             }
         }
     }})
+
+}
